4a9016f8899fe63abbb2c9eae1f22b8a250f020f
imane omari <imane3.omari@gmail.com>
2023-07-29 20:48:45 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+// Function to generate thank you messages
+function writeCards(names, gift) {
+  const thankYouMessages = [];
+  for (const name of names) {
+    thankYouMessages.push(
+      `Thank you, ${name}, for the wonderful ${gift} gift!`
+    );
+  }
+  return thankYouMessages;
+}
+
+// Function to count down from the provided number to zero
+function countDown(startingNumber) {
+  for (let i = startingNumber; i >= 0; i--) {
+    console.log(i);
+  }
+}
